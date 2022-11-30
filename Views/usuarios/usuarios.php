50ba9c588275d47fd8969f5b769111e065ea756d
Jhostyn-2003 <jhostyngavilanez@gmail.com>
2022-11-30 09:48:37 -0500

    archivo usuarios - Para Agregar

---
+++ Views/usuarios/usuarios.php
@@ -0,0 +1,20 @@
+<?php
+    include_once('Views/template/head.php');
+?>
+<body>
+<?php
+   include_once('Views/template/aside.php');
+?>
+<main>
+
+    <div>Holaaa</div>
+    <!--AGREGAR AQUI VA TODO EL FORMULARIO -->
+
+
+</main>
+
+</body>
+
+<?php
+    include_once('Views/template/scripts.php');
+?>
