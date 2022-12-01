0ae5d877fe36ae160b83e9531288698af0fd7d77
SammyEc <samuelto1359@gmail.com>
2022-12-01 17:40:26 -0500

    Merge branch 'main' of https://github.com/Sammy593/CitasMedicas

--- Views/dashboard/dashboard.php
+++ Views/dashboard/dashboard.php
@@ -1,9 +1,9 @@
 <?php
-include_once('../template/head.php');
+include_once('Views/template/head.php');
 ?>
 
 <?php
-include_once('../template/aside.php');
+include_once('Views/template/aside.php');
 ?>
 
 <!-- Espacio para colocar los formularios -->
@@ -16,5 +16,5 @@ include_once('../template/aside.php');
 </div>
 
 <?php
-include_once('../template/scripts.php');
+include_once('Views/template/scripts.php');
 ?>
