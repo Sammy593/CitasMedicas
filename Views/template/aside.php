778623157587e34eb057f1e2b53d4a1f2d166edd
metalDavid <davids14209@gmail.com>
2022-12-02 03:01:03 -0500

    head, aside

--- Views/template/aside.php
+++ Views/template/aside.php
@@ -4,12 +4,12 @@
                 <img src="<?php echo (media()) ?>/img/imgDashboard/logoHome.jpg" alt="logoHome" style="height: 50%; min-height: 20px; max-height: 200px" />
             </div>
             <ul>
-                <li data-id="medicos">Médicos</li>
-                <li data-id="especialidades">Especialidades</li>
-                <li data-id="pacientes">Pacientes</li>
-                <li data-id="asistentes">Asistentes</li>
-                <li data-id="roles">Roles</li>
-                <li data-id="salir">Salir</li>
+                <li data-id="medicos"><a href="<?php echo (views()) ?>/medicos/medicos.php">Médicos</a></li>
+                <li data-id="especialidades"><a href="desconectarLogin.php">Especialidades</a></li>
+                <li data-id="pacientes"><a href="desconectarLogin.php">Pacientes</a></li>
+                <li data-id="asistentes"><a href="desconectarLogin.php">Asistentes</a></li>
+                <li data-id="roles"><a href="desconectarLogin.php">Roles</a> </li>
+                <li data-id="salir"><a href="">Cerrar Sesión</a></li>
             </ul>
         </div>
         <!-- <iframe src="static/views/courses.html" frameborder="0" class="views"></iframe> -->
